9cabd9079e7a0867c0885662df5f13eff24b9e59
Loumber <92634780+Loumber@users.noreply.github.com>
2024-05-01 09:47:41 +0300

    Update data_provider.dart

--- lib/src/features/authentication/data/provider/data_provider.dart
+++ lib/src/features/authentication/data/provider/data_provider.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:secure_link_messenger/src/features/authentication/domain/entities/sign_up/user_sign_up_entity.dart';
 
-class DataLayer {
+class DataProvider {
   final GetIt getIt = GetIt.instance;
 
   UserSignUpEntity getFileFromDomain() {
