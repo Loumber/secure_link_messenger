835b536013f8e5edc716e60999c2c023ddd1b8ed
Loumber <92634780+Loumber@users.noreply.github.com>
2024-05-01 10:43:48 +0300

    add uploadin photo on storage

--- lib/src/features/authentication/data/repositories/authentication_repository_impl.dart
+++ lib/src/features/authentication/data/repositories/authentication_repository_impl.dart
@@ -1,14 +1,18 @@
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:logger/logger.dart';
 import 'package:secure_link_messenger/src/features/authentication/data/provider/data_provider.dart';
 import 'package:secure_link_messenger/src/features/authentication/domain/repositories/authentication_repository.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class AuthenticationRepositoryImpl implements AuthenticationRepository{
  
 
   DataProvider _dataProvider = DataProvider();
 
+  late User _currentUser;
   
+  late String _imageURL;
   
 
   @override
@@ -46,7 +50,13 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository{
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: _dataProvider.getUserFromDomain().email, 
         password: _dataProvider.getUserFromDomain().password);
-      //await dischargePhoto(_dataProvider.getUserFromDomain().photo);
+
+      _currentUser = FirebaseAuth.instance.currentUser!;
+      _currentUser.updateDisplayName(_dataProvider.getUserFromDomain().name);
+
+      _imageURL = await dischargePhoto();
+      
+      _currentUser.updatePhotoURL(_imageURL);
     } on FirebaseAuthException catch(e){
       // ignore: avoid_print
       print(e);
@@ -54,10 +64,20 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository{
   }
   
   @override
-  Future<void> dischargePhoto(File imeage) {
-    // TODO: implement dischargePhoto
-    throw UnimplementedError();
+Future<String> dischargePhoto() async {
+  File file = _dataProvider.getUserFromDomain().photo; 
+  try { 
+    firebase_storage.FirebaseStorage storage =
+      firebase_storage.FirebaseStorage.instance;
+    firebase_storage.Reference ref = storage.ref().child(file.path);
+    await ref.putFile(file);
+
+    return await ref.getDownloadURL();   
+  } catch (e) {
+    print(e);
   }
+  return '';
+}
   
   @override
   Future<String> downloadPhotoUrl() {
--- lib/src/features/authentication/domain/repositories/authentication_repository.dart
+++ lib/src/features/authentication/domain/repositories/authentication_repository.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
-
 abstract class AuthenticationRepository {
   
-  Future<void> dischargePhoto(File imeage);
+  Future<String> dischargePhoto();
   Future<String> downloadPhotoUrl();
   Future<bool> checkUser(String email);
   Future<bool> signIn(String email, String password);
